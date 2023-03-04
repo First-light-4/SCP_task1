75460124cce12856b5601e994cfcef9ac3c1cf62
Valentine Savchenko <savchenkovs99@gmail.com>
2023-03-04 19:12:41 +0300

    add all files

--- png_to_jpeg.py
+++ png_to_jpeg.py
@@ -8,9 +8,7 @@ def main(path):
         im1.save(rf'{path}\{i.split(".")[0]}.jpeg')
         os.remove(rf'{path}\{i}')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     path = r'C:\Users\vsavc\OneDrive\Desktop\GIT\SCP_task1\people'
     main(path)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
